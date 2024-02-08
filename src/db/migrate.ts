6006b902f78957749ce31379faf8c50dfdc89202
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-02-08 19:05:10 +0200

    Update database migration script and add initial migration

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,20 +1,22 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
-import {neon} from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
 
 const sql = neon(process.env.DATABASE_URL);
 
 const db = drizzle(sql);
 
 const main = async () => {
-  try {
-    await migrate(db, { migrationsFolder: "drizzle" });
+	try {
+		await migrate(db, {
+			migrationsFolder: "src/db/migrations",
+		});
 
-    console.log("Migration successful");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+		console.log("Migration successful");
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
 };
 
 main();
